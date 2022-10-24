bc9a6c90f4333ae6a34118b7fbecdd51f0742ee6
Dima Aksyonov <dmi.aksyonov@gmail.com>
2022-10-24 19:00:54 +0300

    cell colors

---
+++ NotesTest/Extensions/UIColor+CellColors.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor+CellColors.swift
+//  NotesTest
+//
+//  Created by Dmitry Aksyonov on 24.10.2022.
+//
+
+import UIKit
+
+// MARK: - NoteListCell Colors
+
+extension UIColor {
+
+    static let bark = UIColor(named: "bark")
+    static let barkLI = UIColor(named: "barkLI")
+    static let barkLR = UIColor(named: "barkLR")
+    static let leaf = UIColor(named: "leaf")
+    static let leafLI = UIColor(named: "leafLI")
+    static let leafLR = UIColor(named: "leafLR")
+    static let skies = UIColor(named: "skies")
+    static let skiesLI = UIColor(named: "skiesLI")
+    static let skiesLR = UIColor(named: "skiesLR")
+    static let sun = UIColor(named: "sun")
+    static let sunLI = UIColor(named: "sunLI")
+    static let sunLR = UIColor(named: "sunLR")
+}
